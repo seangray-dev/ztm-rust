669dc55f4c393020efff0bd6c36ca3d290d93310
Sean Gray <seangray.dev@gmail.com>
2023-11-04 08:39:33 -0400

    complete a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -5,7 +5,16 @@
 //
 // Notes:
 // * Use a function to add two numbers together
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
 // * Use a function to display the result
+fn display_sum(sum: i32) {
+    println!("{:?}", sum);
+}
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+  let result = sum(1, 2);
+  display_sum(result);
+}
